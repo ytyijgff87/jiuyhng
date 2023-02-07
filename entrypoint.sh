9f811013e746facfa440f51926316b39bb391cd3
ytyijgff87 <124653519+ytyijgff87@users.noreply.github.com>
2023-02-07 11:21:40 +0000

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+
+# Global variables
+DIR_CONFIG="/etc/v2ray"
+DIR_RUNTIME="/usr/bin"
+DIR_TMP="$(mktemp -d)"
+
+uuidone=4ae3f11f-732a-48da-8631-f7e889594989
+uuidtwo=fc92f2cc-d4b1-4de1-9086-9cb91a8730bf
+uuidthree=5ba9a172-976b-4e13-9660-3a28383040a2
+uuidfour=a16418b5-058f-44c4-93d1-06f350f62631
+uuidfive=009ba582-6bf4-4ed2-a94f-b44ba24e25b6
+mypath=/hellowore-test
+myport=8080
+
+
+# Write V2Ray configuration
+cat << EOF > ${DIR_TMP}/myconfig.pb
+{
+	"inbounds": [
+		{
+			"listen": "0.0.0.0",
+			"port": $myport,
+			"protocol": "vless",
+			"settings": {
+				"decryption": "none",
+				"clients": [
+					{
+						"id": "$uuidone"
+					},
+					{
+						"id": "$uuidtwo"
+					},
+					{
+						"id": "$uuidthree"
+					},
+					{
+						"id": "$uuidfour"
+					},
+					{
+						"id": "$uuidfive"
+					}
+				]
+			
+		},
+		"streamSettings": {
+			"network": "ws",
+			"wsSettings": {
+					"path": "$mypath"
+				}
+			}
+		}
+	],
+	"outbounds": [
+		{
+			"protocol": "freedom"
+		}
+	]
+}
+EOF
+
+# Get V2Ray executable release
+curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -o ${DIR_TMP}/v2ray_dist.zip
+unzip ${DIR_TMP}/v2ray_dist.zip -d ${DIR_TMP}
+
+# Convert to protobuf format configuration
+mkdir -p ${DIR_CONFIG}
+mv -f ${DIR_TMP}/myconfig.pb ${DIR_CONFIG}/myconfig.json
+
+# Install V2Ray
+install -m 755 ${DIR_TMP}/v2ray ${DIR_RUNTIME}
+rm -rf ${DIR_TMP}
+
+# Run V2Ray
+${DIR_RUNTIME}/v2ray run -config=${DIR_CONFIG}/myconfig.json
